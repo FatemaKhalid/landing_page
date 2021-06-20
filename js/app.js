3feb1e0ee63b5277e90f696973c4d4d5a5005deb
FatemaKhalid <fatemakhalid96@gmail.com>
2021-06-20 23:43:16 +0200

    Adding dynamic navBar

---
+++ js/app.js
@@ -0,0 +1,49 @@
+function createNavList() {
+    const navUl = document.getElementById('nav-list');
+    const navSections = document.querySelectorAll('.section');
+    for (const sec of navSections) {
+        const li = document.createElement('li');
+        li.classList.add('nav-list__item');
+        const anch = document.createElement('a');
+        anch.classList.add('nav-list__link')
+        anch.href = '#' + sec.id;
+        anch.innerText = sec.id.replaceAll('_', ' ').replace(/\b\w/g, c => c.toUpperCase());
+        li.append(anch);
+        navUl.append(li);
+        li.addEventListener('click', () => {
+            setTimeout(() => {
+                removeHashFromURL();
+            }, 5);
+        }
+
+        );
+    }
+
+}
+
+function removeHashFromURL() {
+    history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+}
+function toTopScroll() {
+    let toTopBtn = document.getElementById('top-btn__content--desk');
+
+    toTopBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+
+    let scrollPosition = 0;
+    window.addEventListener('scroll', function () {
+        if ((document.body.getBoundingClientRect()).top > scrollPosition) {
+            document.getElementById('top-btn').classList.remove('active');
+        } else {
+            document.getElementById('top-btn').classList.add('active');
+        }
+        scrollPosition = (document.body.getBoundingClientRect()).top;
+    });
+}
+
+function initializePage() {
+    createNavList();
+    toTopScroll();
+}
